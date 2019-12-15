21228496c88ce09584aec198f3fb85192d569b4e
frantchessico <jaimeinoque20@gmail.com>
2019-12-15 14:02:24 +0200

    primerio commit de todoDragDropPWA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,21 +1,15 @@
 export const environment = {
   production: true,
-  firebase:  {
-    // apiKey: 'AIzaSyAKlEX5a8PQaadYNSrohqfl33O37Kjkv1Y',
-    // authDomain: 'angular-voxer.firebaseapp.com',
-    // databaseURL: 'https://angular-voxer.firebaseio.com',
-    // projectId: 'angular-voxer',
-    // storageBucket: 'angular-voxer.appspot.com',
-    // messagingSenderId: '122050526979',
-    // appId: '1:122050526979:web:529304dce5ffca7a'
-
-
-    apiKey: 'AIzaSyDrZH3Oou9-p3jNomEG6Dgjm2ExONgQang',
-    authDomain: 'naminhaloja.firebaseapp.com',
-    databaseURL: 'https://naminhaloja.firebaseio.com',
-    projectId: 'naminhaloja',
-    storageBucket: 'naminhaloja.appspot.com',
-    messagingSenderId: '663817652034',
-    appId: '1:663817652034:web:1f5c86eeddb3aa96'
+  firebase: {
+    apiKey: '#',
+    authDomain: '#',
+    databaseURL: '#',
+    projectId: '#',
+    storageBucket: '#',
+    messagingSenderId: '#',
+    appId: '#',
+    measurementId: '#'
   }
 };
+
+
